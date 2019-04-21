cd084207c552532e4c16c41e31cd145a06d540f8
ChaiYuan <31722648@qq.com>
2019-04-21 17:40:10 +0800

    heder

--- travel/src/pages/home/Home.vue
+++ travel/src/pages/home/Home.vue
@@ -1,12 +1,17 @@
 <template>
 <div>
-    home
+    <home-header></home-header>
     
 </div>
 </template>
 <script>
+import HomeHeader from './components/Header'
+
     export default{
         name:'Home',
+        components:{
+            HomeHeader
+        }
     }
 </script>
 <style>
