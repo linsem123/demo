5d17b41aaa83e462fe4670fd8e5bcf62be5299e4
ChaiYuan <31722648@qq.com>
2019-04-23 23:18:53 +0800

    change

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,8 +1,26 @@
-<template></template>
+<template>
+    <div class=".icons">
+        <div class="icon">
+            <img src=""/>
+            <p></p>
+        </div>
+    </div>
+</template>
 <script>
     export default{
         name:'HomeIcons'
     }
 </script>
 <style lang="stylus" scoped>
+    .icon 
+        overflow:hidden
+        height:0   
+        padding-bottom :50%
+        background :green
+        .icon
+            float:left
+            width:25%
+            padding-bottom :25%
+
+
 </style>
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -35,7 +35,7 @@ export default{
       overflow: hidden
       width: 100%
       height: 0
-      padding-bottom: 31.25%
+      padding-bottom: 26.67%
       background: #eee
       .swiper-img
         width: 100%
